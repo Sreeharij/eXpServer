7949af846f9f1424e11b7670eccdb2dd77e2304d
sreeharij <sreeharij2003@gmail.com>
2025-10-04 15:32:48 +0530

    tcp server made iterative

--- phase_0/tcp_server.c
+++ phase_0/tcp_server.c
@@ -7,64 +7,90 @@
 #include <sys/socket.h>
 #include <unistd.h>
 
-
 #define PORT 8080
 #define BUFF_SIZE 10000
-#define MAX_ACCEPT_BACKLOG 5
+#define MAX_ACCEPT_BACKLOG 2
 
+void strrev(char *str) {
+    int len = strlen(str);
+    int end = len - 1;
 
-void strrev(char *str){
-    for(int start = 0, end=strlen(str) - 2; start < end; start++,end--){
+    if (len > 0 && str[end] == '\n') {
+        end--;
+    }
+
+    for (int start = 0; start < end; start++, end--) {
         char temp = str[start];
         str[start] = str[end];
         str[end] = temp;
     }
 }
 
-int main(){ 
+int main() {
     int listen_sock_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (listen_sock_fd < 0) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
 
     int enable = 1;
     setsockopt(listen_sock_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
 
     struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
 
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = htonl(INADDR_ANY);
     server_addr.sin_port = htons(PORT);
 
-    bind(listen_sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr));
+    if (bind(listen_sock_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0) {
+        perror("bind");
+        close(listen_sock_fd);
+        exit(EXIT_FAILURE);
+    }
 
-    listen(listen_sock_fd, MAX_ACCEPT_BACKLOG);
+    if (listen(listen_sock_fd, MAX_ACCEPT_BACKLOG) < 0) {
+        perror("listen");
+        close(listen_sock_fd);
+        exit(EXIT_FAILURE);
+    }
 
     printf("[INFORMATION] Server listening on port %d\n", PORT);
 
-    struct sockaddr_in client_addr;
-    socklen_t client_addr_len;
+    while (1) {
+        struct sockaddr_in client_addr;
+        socklen_t client_addr_len = sizeof(client_addr);
 
-    int conn_sock_fd = accept(listen_sock_fd, (struct sockaddr *)&client_addr, &client_addr_len);
-    printf("[INFORMATION] Client connected to Server\n");
-
-    while(1){
-        char buff[BUFF_SIZE];
-        memset(buff,0, BUFF_SIZE);
-
-        ssize_t read_n = recv(conn_sock_fd, buff, sizeof(buff), 0);
-        
-        if(read_n < 0){
-            printf("[INFORMATION] Error occured. Closing server\n");
-            close(conn_sock_fd);
-            exit(1);
-        }
-        else if(read_n == 0){
-            printf("[INFORMATION] Client Disconnected. Closing server\n");
-            close(conn_sock_fd);
-            exit(1);
+        int conn_sock_fd = accept(listen_sock_fd, (struct sockaddr *)&client_addr, &client_addr_len);
+        if (conn_sock_fd < 0) {
+            perror("accept");
+            continue;
         }
 
-        printf("[CLIENT MESSAGE] %s", buff);
-        strrev(buff);
-        send(conn_sock_fd, buff, read_n, 0);
+        printf("[INFORMATION] Client connected to Server\n");
 
+        while (1) {
+            char buff[BUFF_SIZE];
+            memset(buff, 0, BUFF_SIZE);
+
+            ssize_t read_n = recv(conn_sock_fd, buff, sizeof(buff), 0);
+
+            if (read_n < 0) {
+                perror("[ERROR] recv");
+                close(conn_sock_fd);
+                break;  
+            } else if (read_n == 0) {
+                printf("[INFORMATION] Client disconnected.\n");
+                close(conn_sock_fd);
+                break;
+            }
+
+            printf("[CLIENT MESSAGE] %s", buff);
+            strrev(buff);
+            send(conn_sock_fd, buff, read_n, 0);
+        }
     }
+
+    close(listen_sock_fd);
+    return 0;
 }
